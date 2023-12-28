b5e47e319f882f6e0f7241e4b8de44fb274534ec
Mark Bednarczyk <mark@slytechs.com>
2023-12-28 17:02:51 -0500

    Refactored java package again

--- src/main/java/com/slytechs/jnet/jnetpcap/example/Example4_LiveCaptureAndPrint.java
+++ src/main/java/com/slytechs/jnet/jnetpcap/example/Example4_LiveCaptureAndPrint.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.slytechs.jnet.jnetpcap.examples;
+package com.slytechs.jnet.jnetpcap.example;
 
 import java.util.List;
 
--- src/main/java/com/slytechs/jnet/jnetpcap/example/usecase/UsecaseIpOptions.java
+++ src/main/java/com/slytechs/jnet/jnetpcap/example/usecase/UsecaseIpOptions.java
@@ -15,7 +15,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.slytechs.jnet.jnetpcap.examples.usecase;
+package com.slytechs.jnet.jnetpcap.example.usecase;
 
 import org.jnetpcap.PcapException;
 
